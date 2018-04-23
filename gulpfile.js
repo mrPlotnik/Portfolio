6aa40355fac3f46ae4e273487486911d2b0e1986
Andrey Plotnikov (PC) <plotnik1992@gmail.com>
2018-04-23 13:37:23 +0300

    Реализовал оптимизацию изображений, + много мелких правок, еомментов

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
-var gulp 					= require('gulp'), // Подключаем Gulp
+var 
+		// Gulp
+		gulp 					= require('gulp'), // Подключаем Gulp
+		// Gulp plugins		 
 		pug 					= require('gulp-pug'), // Подключаем Pug
-		sass 					= require('gulp-sass'), //Подключаем Sass пакет,		
-		browserSync		= require('browser-sync'), // Подключаем Browser Sync
-		reload				= browserSync.reload,
-		concat 				= require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов). 
+		sass 					= require('gulp-sass'), //Подключаем Sass пакет,
+		concat 				= require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов).
 		uglify 				= require('gulp-uglify'), // Минимизируем наш common.js 
 		rename 				= require('gulp-rename'), // Подключаем библиотеку для переименования файлов
 		autoprefixer 	= require('gulp-autoprefixer'), // Подключаем библиотеку для автоматического добавления префиксов
-		cleanCSS 			= require('gulp-clean-css'); //
+		csso 					= require('gulp-csso'), // Подключаем отличный CSS компрессор
+		imagemin 			= require('gulp-imagemin'), // Оптимизируем картинки
+		filesize 			= require('gulp-size'), // Узнаем размер файла
+		// Utilities
+		browserSync		= require('browser-sync'), // Подключаем Browser Sync //
+		reload				= browserSync.reload; 
+
 
 //-------------------------------------------
 // Компилируем Pug в HTML
@@ -22,36 +29,44 @@ gulp.task('pug', function () {
 });
 
 //------------------------------------------
-// Компиируем SASS в CSS
-// 1. Переименовываем, добавляем префиксы,
+// Компилируем SASS в CSS
+// 1. Читаемый вариант
+// 2. Переименовываем, добавляем префиксы,
 // минифицируем
-// 2. Читаемый вариант
 //------------------------------------------
-gulp.task('sass', function(){ 
-	// 1.
-	return gulp.src('app/sass/**/*.sass') 
-		.pipe(sass().on('error', sass.logError)) // Оповещение в случае ошибки при компиляции SASS в CSS
-		.pipe(rename({suffix: '.min', prefix : ''})) // Добавление суффикса и префикса в название CSS файла
+gulp.task('sass', function(){ 	
+	return gulp.src('app/sass/**/*.sass')		
+		.pipe(sass({
+			outputStyle: 'expand', 
+			includePaths: require('node-bourbon').includePaths
+		}).on('error', sass.logError)) // Оповещение в случае ошибки при компиляции SASS в CSS
 		.pipe(autoprefixer(['last 15 versions'])) // Добавление автопрефиксов, для одинакового отображения во всех браузерах (последнии 15 версий)
-		.pipe(cleanCSS()) // Очистить и минифицировать
-		.pipe(gulp.dest('app/css/min')) 
-	// 2.	
-		return gulp.src('app/sass/**/*.sass') 
-		.pipe(sass().on('error', sass.logError)) 	
-		.pipe(autoprefixer(['last 15 versions'])) 
-	// Inject	
-		.pipe(browserSync.stream()) 
+		.pipe(filesize())		
+		.pipe(gulp.dest('app/css'))		
+		.pipe(csso())	// Минимизируем	 		
+		.pipe(rename({suffix: '.min', prefix : ''})) // Добавление суффикса и префикса в название CSS файла
+		.pipe(gulp.dest('app/css/min'))
+		.pipe(filesize())			
+		.pipe(browserSync.stream()); // Inject	
 });
 
 //--------------------------------------------
 // Минимизируем наш common.js 
 //--------------------------------------------
-gulp.task('common-js', function() {
-	return gulp.src('app/js/common.js')	
-	.pipe(rename({suffix: '.min', prefix : ''})) // Добавляем суффикс и префикс в название CSS файла
-	.pipe(uglify())
-	.pipe(gulp.dest('app/js'))
-	.pipe(reload({stream: true}));	// Reload
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/jquery-2.1.3.min.js',
+		'app/libs/waypoints/waypoints.min.js',
+		'app/libs/magnific-popup/js/jquery.magnific-popup.min.js',
+		'app/libs/parallax/parallax.min.js',
+		'app/libs/pagescroll2id/PageScroll2id.min.js',
+		'app/js/common.js',
+		])		
+		.pipe(concat('scripts.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'))
+		.pipe(filesize())	
+		.pipe(reload({stream: true}));	// Reload
 });
 
 //---------------------------------------------
@@ -69,10 +84,23 @@ gulp.task('browser-sync', function() {
 //----------------------------------------------
 // Наблюдаем за изменениями, компилируем, перезагружаем
 //----------------------------------------------
-gulp.task('watch', ['pug', 'sass', 'common-js', 'browser-sync'], function() {
+gulp.task('watch', ['pug', 'sass', 'js', 'browser-sync'], function() {
 	gulp.watch('app/sass/**/*.sass', ['sass']);
 	gulp.watch('app/views/**/*.pug', ['pug']);
-	gulp.watch('app/js/**/*.js', ['common-js']);
+	gulp.watch('app/js/**/*.js', ['js']);
 });
 
-gulp.task('default', ['watch']);
+//----------------------------------------------
+// Оптимизация, минификация изображений
+//----------------------------------------------
+gulp.task('imagemin', () =>
+	gulp.src('app/img/stock/**/*')	
+		.pipe(imagemin())
+		.pipe(gulp.dest('app/img/'))
+);
+
+//----------------------------------------------
+// По умолчанию (при запуске)
+//----------------------------------------------
+gulp.task('default', ['imagemin', 'watch', ]
+);
