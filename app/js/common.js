09735906c1a96c2cb28d2624cdff4a2d15c74f9f
Andrey Plotnikov <plotnik1992@gmail.com>
2022-10-20 00:00:43 +0700

    Много оптимизации и адаптации

--- app/js/common.js
+++ app/js/common.js
@@ -1,88 +1,102 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const body = document.querySelector('body');
-	const burger = document.querySelector('.burger');
-	const menu = document.querySelector('.menu');
-	const links = document.querySelectorAll('.menu a');
-	let pro = burger.getAttribute('aria-expanded');
 
-	burger.addEventListener('click', function() {
-		body.classList.toggle('stop-scroll');
+	function burger() {
+		const body = document.querySelector('body');
+		const burger = document.querySelector('.burger');
+		const menu = document.querySelector('.menu');
+		const links = document.querySelectorAll('.menu a');
+		let pro = burger.getAttribute('aria-expanded');
 
-		if (pro == 'false') {
-			burger.setAttribute('aria-expanded', 'true');
-			pro = 'true';
-		} else {
-			burger.setAttribute('aria-expanded', 'false');
-			pro = 'false';
+		function isActive() {
+			if (pro == 'false') {
+				burger.setAttribute('aria-expanded', 'true');
+				pro = 'true';
+				links.forEach((el) => {
+					el.classList.add('fadeInUp', 'animated');
+				})
+			} else {
+				burger.setAttribute('aria-expanded', 'false');
+				pro = 'false';
+				links.forEach((el) => {
+					el.classList.remove('fadeInUp', 'animated');
+				})
+			}
 		}
 
-		burger.classList.toggle('burger--active');
-		menu.classList.toggle('menu--active');
-		links.forEach(function(el,) {
-			el.classList.add('fadeInUp', 'animated');
+		burger.addEventListener('click', function() {
+			body.classList.toggle('stop-scroll');
+			burger.classList.toggle('burger--active');
+			menu.classList.toggle('menu--active');
+			isActive();
 		})
-	})
-});
 
+		links.forEach((el, index) => {
+			el.addEventListener('click', function(e) {
+				body.classList.toggle('stop-scroll');
+				burger.classList.toggle('burger--active');
+				menu.classList.toggle('menu--active');
+				isActive();
+			})
+		})
 
+	}
 
-
-$(document).ready(function() {
+	burger();
 
 	$(window).stellar();
 
-	//-----------------------------------------------------
 	// Анимация
+	$(".header__logo").addClass("flipInY animated").css("animation-delay", "0.5s")	;
+	$(".burger").addClass("fadeIn animated").css("animation-delay", "1.5s");
+	$(".hero__link").addClass("fadeIn animated").css("animation-delay", "0.5s");
+	$(".hero__descr:first-child").addClass("fadeInDown animated").css("animation-delay", "1s");
+	$(".hero__descr:last-child").addClass("fadeInUp animated").css("animation-delay", "1s");
+	$(".hero__mouse-icon").addClass("fadeInUp animated").css("animation-delay", "2s");
+
 	//-----------------------------------------------------
-	$(window).load(function() {
-		// Header animation
-		$(".loader_inner").fadeOut();
-		$(".loader").delay(400).fadeOut("slow");
-		// $(".logo-container").addClass("flipInY animated").css("animation-delay", "1.5s")	;
-		// $(".logo-text").addClass("fadeIn animated").css("animation-delay", "2s");
-		// $(".content h1").addClass("fadeInDown animated").css("animation-delay", "0.5s");
-		// $(".content p").addClass("fadeInUp animated").css("animation-delay", "1s");
-		// $(".mw-wrap").addClass("fadeIn animated").css("animation-delay", "2.5s");
+	//Animate CSS + WayPoints javaScript Plugin
+	//Example: $(".element").animated("zoomInUp", "zoomOutDown");
+	//-----------------------------------------------------
 
-		//-----------------------------------------------------
-		//Animate CSS + WayPoints javaScript Plugin
-		//Example: $(".element").animated("zoomInUp", "zoomOutDown");
-		//-----------------------------------------------------
-		(function($) {
-			$.fn.animated = function(inEffect, outEffect) {
-				$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
-					if (dir === "down") {
-						$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
-					} else {
-						$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
-					};
-				}, {
-					offset: "80%"
-				}).waypoint(function(dir) {
-					if (dir === "down") {
-						$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
-					} else {
-						$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
-					};
-				}, {
-					offset: -$(window).height()
-				});
-			};
-		})(jQuery);
+	(function($) {
+		$.fn.animated = function(inEffect, outEffect) {
+			$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+				if (dir === "down") {
+					$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+				} else {
+					$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+				};
+			}, {
+				offset: "80%"
+			}).waypoint(function(dir) {
+				if (dir === "down") {
+					$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+				} else {
+					$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+				};
+			}, {
+				offset: -$(window).height()
+			});
+		};
+	})(jQuery);
 
-		$(".animation_1").animated("flipInY", "fadeOut");
-		$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
-		$(".animation_3").animated("fadeInRight", "fadeOutRight");
-		$(".animation_4").animated("fadeIn", "fadeOut");
+	$(".animation_1").animated("flipInY", "fadeOut");
+	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
+	$(".animation_3").animated("fadeInRight", "fadeOutRight");
+	$(".animation_4").animated("fadeIn", "fadeOut");
 
-		//-----------------------------------------------------
-		// Header. Плавный скролл по клику на иконку мыши
-		//-----------------------------------------------------
-		// $(".mouse-icon").click(function() {
-		// 	$("html, body").animate({
-		// 		scrollTop  : $("#about").offset().top
-		// 	}, 800);
-		// });
+	// Плавный скролл
+	$('.smooth-scroll').on( 'click', function() {
+		var el = $(this);
+		var dest = el.attr('href'); // получаем направление
+		if(dest !== undefined && dest !== '') { // проверяем существование
+				$('html').animate({
+					scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+				}, 500 // скорость прокрутки
+				);
+		}
+		return false;
+	});
 
 		//-----------------------------------------------------
 		// magnificPopup
@@ -101,55 +115,5 @@ $(document).ready(function() {
 		// 	},
 		// });
 
-		//-----------------------------------------------------
-		// Меню сайта
-		//-----------------------------------------------------
-
-
-
-		// $(".top_mnu ul a").click(function() {
-		// 	$(".top_mnu").fadeOut(600);
-		// 	$(".sandwich").toggleClass("active");
-		// 	$(".top_text").css("opacity", "1");
-		// });
-
-		// $(".toggle_mnu").click(function() {
-		// 	if ($(".top_mnu").is(":visible")) {
-		// 		$(".top_text").css("opacity", "1");
-		// 		$(".top_mnu").fadeOut(600);
-		// 		$(".top_mnu li a").removeClass("fadeInUp animated");
-		// 	} else {
-		// 		$(".top_text").css("opacity", ".1");
-		// 		$(".top_mnu").fadeIn(600);
-		// 		$(".top_mnu").css("display", "table");
-		// 		$(".top_mnu li a").addClass("fadeInUp animated");
-		// 	};
-		// });
-
-		// $(".top_mnu ul a").mPageScroll2id();
-		// $(".scroll2id").mPageScroll2id();
-
-		//-----------------------------------------------------
-		//Все element одинаковой высоты
-		//-----------------------------------------------------
-		// function equalHeight(element){
-		// 	var maxHeight = 0;  // максимальная высота, первоначально 0
-		// 	element.each(function() { // цикл "для каждого из элементов"
-		// 		if($(this).height() > maxHeight) { // если высота колонки больше значения максимальной высоты,
-		// 			maxHeight = $(this).height();	// то она сама становится новой максимальной высотой
-		// 		}
-		// 	});
-		// 	$(element).height(maxHeight); // устанавливаем высоту каждой колонки равной значению максимальной высоты
-		// };
-
-		// var element = $(".card-body"); // Высоту чего будем выравнивать?
-		// equalHeight(element);	// Передаем функции нужные элементы
-
-		// $(window).resize(function	() { // пересчитываем значения при изменении размераокна браузера
-		// 	element.css("height", "100%"); // Не забываем сбрасывать
-		// 	equalHeight(element);
-		// });
-
-	});
 
 });
